c5a0b62da5c9839dcc01f4092243c45dce1787f3
alireza koohzad <alirezakhd2705@gmail.com>
2022-07-29 18:14:11 +0430

    ...

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,21 +1,29 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
+import { Serialize, SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { updateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
 
     constructor(private userService : UsersService){}
 
     @Post('signup')
     async createUser (@Body() body : CreateUserDto) {
-        await this.userService.create(body.email , body.password); 
+        const user = await this.userService.create(body.email , body.password); 
+        console.log(user);
+        return user
+        
     } 
 
     @Get(':id')
      async findUser(@Param('id') id : string){
-        return await this.userService.findOne(parseInt(id))
+        const user =  await this.userService.findOne(parseInt(id))
+        console.log(user);
+        return user;
     }
 
     @Get()
