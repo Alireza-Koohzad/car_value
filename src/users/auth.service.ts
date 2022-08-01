613d5d77448011be90c9b9d241402c49df679459
alireza koohzad <alirezakhd2705@gmail.com>
2022-08-01 21:27:19 +0430

    implementing signup user route with auth service

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,13 +1,25 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
+import * as bcrypt from 'bcrypt';
 import { UsersService } from "./users.service";
-
 @Injectable()
 export class AuthService {
 
     constructor(private userService : UsersService){};
 
-    async signup(){
+    async signup(email : string  , password : string){
+        //check exist email
+        const user = await this.userService.find(email);
+        if(user){
+            new BadRequestException("this user has  already exist");
+        }
+
+        //hashing password
+        const hashPassword = await bcrypt.hash(password , 10);        
         
+        //create new user
+        const newUser = await this.userService.create(email , hashPassword);
+        
+        return newUser;
     }
 
 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
 import { Serialize, SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import { AuthService } from './auth.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { updateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
@@ -9,15 +10,15 @@ import { UsersService } from './users.service';
 @Serialize(UserDto)
 export class UsersController {
 
-    constructor(private userService : UsersService){}
+    constructor(private userService : UsersService , private authService : AuthService){}
 
     @Post('signup')
     async createUser (@Body() body : CreateUserDto) {
-        const user = await this.userService.create(body.email , body.password); 
-        console.log(user);
-        return user
-        
+        const user =await  this.authService.signup(body.email , body.password);        
+        return user;
     } 
+
+    
 
     @Get(':id')
      async findUser(@Param('id') id : string){
